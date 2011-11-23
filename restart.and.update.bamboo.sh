9ef288bd96132a80e87d20a3962288cc81e37601
noamt <noamt@jfrog.org>
2011-11-23 16:58:34 +0200

    Fix dev script command executions

--- restart.and.update.bamboo.sh
+++ restart.and.update.bamboo.sh
@@ -9,21 +9,23 @@ if [ -z "$BAMBOO_HOME_DIR" ]; then
 fi
 
 echo "Stopping Bamboo instance"
-sh `$BAMBOO_INSTALL_DIR/bamboo.sh` stop
+sh $BAMBOO_INSTALL_DIR/bamboo.sh stop
 
 echo "Cleaning existing plugin and dependencies"
-rm `$BAMBOO_INSTALL_DIR/webapp/WEB-INF/lib/bamboo-artifactory-plugin*.jar`
-rm -rf `$BAMBOO_HOME_DIR/xml-data/build-dir/org.jfrog.bamboo.bamboo-artifactory-plugin-*`
+rm $BAMBOO_INSTALL_DIR/webapp/WEB-INF/lib/bamboo-artifactory-plugin*.jar
+rm -rf $BAMBOO_HOME_DIR/xml-data/build-dir/org.jfrog.bamboo.bamboo-artifactory-plugin-*
 
 echo "Building new plugin"
 mvn clean && mvn package
 
 echo "Updating Bamboo"
-cp target/bamboo-artifactory-plugin-1.5.x-SNAPSHOT.jar `$BAMBOO_INSTALL_DIR/webapp/WEB-INF/lib/`
+cp target/bamboo-artifactory-plugin-1.5.x-SNAPSHOT.jar "$BAMBOO_INSTALL_DIR/webapp/WEB-INF/lib/"
 
 if [ "$1" = "debug" ]; then
     export BAMBOO_OPTIONS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+else
+    export BAMBOO_OPTIONS=""
 fi
 
 echo "Starting Bamboo instance"
-sh `$BAMBOO_INSTALL_DIR/bamboo.sh` start
+sh $BAMBOO_INSTALL_DIR/bamboo.sh start
