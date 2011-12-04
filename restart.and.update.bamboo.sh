4d66097f196518df5a90862a150b8f6805a1adb6
noamt <noamt@jfrog.org>
2011-12-04 13:39:30 +0200

    * Depending on latest Gradle snapshot for GAP-113 * Improving dev script

--- restart.and.update.bamboo.sh
+++ restart.and.update.bamboo.sh
@@ -5,15 +5,15 @@ if [ -z "$BAMBOO_INSTALL_DIR" ]; then
 fi
 
 if [ -z "$BAMBOO_HOME_DIR" ]; then
-    BAMBOO_HOME_DIR="~/bamboo-home"
+    BAMBOO_HOME_DIR="$HOME/bamboo-home"
 fi
 
 echo "Stopping Bamboo instance"
 sh $BAMBOO_INSTALL_DIR/bamboo.sh stop
 
 echo "Cleaning existing plugin and dependencies"
-rm $BAMBOO_INSTALL_DIR/webapp/WEB-INF/lib/bamboo-artifactory-plugin*.jar
-rm -rf $BAMBOO_HOME_DIR/xml-data/build-dir/org.jfrog.bamboo.bamboo-artifactory-plugin-*
+rm "$BAMBOO_INSTALL_DIR/webapp/WEB-INF/lib/"bamboo-artifactory-plugin*.jar
+find "$BAMBOO_HOME_DIR/xml-data/build-dir" -name org.jfrog.bamboo.bamboo-artifactory-plugin* | xargs rm -rf
 
 echo "Building new plugin"
 mvn clean && mvn package
