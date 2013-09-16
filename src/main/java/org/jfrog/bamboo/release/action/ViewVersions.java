542a682646a71fc9129d5c885b0f4f8809cc3373
Shay Yaakov <shayy@jfrog.com>
2013-09-16 12:09:37 +0300

    BAP-172 - Support Bamboo 5.1

--- src/main/java/org/jfrog/bamboo/release/action/ViewVersions.java
+++ src/main/java/org/jfrog/bamboo/release/action/ViewVersions.java
@@ -101,7 +101,6 @@ public class ViewVersions extends BuildActionSupport {
                 }
             }
         }
-        ActionContext.getContext().getValueStack().push(versions);
         return versions;
     }
 
