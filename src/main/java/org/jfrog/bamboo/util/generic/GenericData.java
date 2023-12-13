52c8d317108b99f5974375a64477c5c076b82314
Yahav Itzhak <yahavi@users.noreply.github.com>
2023-12-13 16:12:11 +0200

    Update build-info and bamboo core (#208)

--- src/main/java/org/jfrog/bamboo/util/generic/GenericData.java
+++ src/main/java/org/jfrog/bamboo/util/generic/GenericData.java
@@ -1,13 +1,10 @@
 package org.jfrog.bamboo.util.generic;
 
-import com.thoughtworks.xstream.annotations.XStreamAlias;
 import org.jfrog.build.extractor.ci.BuildInfo;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.jfrog.build.api.BuildBean.ROOT;
 
 /**
  * This class goal is to represent JSON structure that will be share between
@@ -16,10 +13,7 @@ import static org.jfrog.build.api.BuildBean.ROOT;
  * @author Lior Hasson
  */
 
-@XStreamAlias(ROOT)
 public class GenericData implements Serializable {
-
-    @XStreamAlias("build")
     private List<BuildInfo> builds;
 
     public List<BuildInfo> getBuilds() {
